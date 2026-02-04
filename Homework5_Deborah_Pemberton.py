0aaa4242be71101c3d1381fb0ab48aa16edf3d62
dpembe2 <dpembe2@uw.edu>
2026-02-03 23:37:55 -0800

    Update Homework5_Deborah_Pemberton.py

--- Homework5_Deborah_Pemberton.py
+++ Homework5_Deborah_Pemberton.py
@@ -2,11 +2,11 @@
 cats =  ['Siamese', 'Tabby', 'Maine Coon', 'Ragdoll', 'American Shorthair', 'Abyssinian']
 print("cats")
 first_two = cats[:2]
-print(f"The first two items in the list are:", *first_two, sep=', ' )
+print(f"The first two items in the list are: " + ", ".join(first_two)) 
 list_length = len(cats)
 middle_index = list_length // 2
 middle_two_items = cats[middle_index - 1 : middle_index + 1]
-print(f"The middle two items in the list are:", *middle_two_items, sep=', ')
+print(f"The middle two items in the list are: " + ", ".join(middle_two_items)) 
 print(f"The first and last items in the list are: {cats[0]}, {cats[-1]}")
 #Tuple Exercise:
 my_tuple = ('taco', 'mashed potatoes', 'beef stew', 'french toast','chef salad')
