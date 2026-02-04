110a4a0babd988c984a44df101672da25b942011
dpembe2 <dpembe2@uw.edu>
2026-02-04 00:15:06 -0800

    Update Homework5_Deborah_Pemberton.py

--- Homework5_Deborah_Pemberton.py
+++ Homework5_Deborah_Pemberton.py
@@ -1,6 +1,6 @@
 #List Exercise
 cats =  ['Siamese', 'Tabby', 'Maine Coon', 'Ragdoll', 'American Shorthair', 'Abyssinian']
-print("cats")
+print(*cats, sep = ", ")
 first_two = cats[:2]
 print(f"The first two items in the list are: " + ", ".join(first_two)) 
 list_length = len(cats)
@@ -11,11 +11,11 @@ print(f"The first and last items in the list are: {cats[0]}, {cats[-1]}")
 #Tuple Exercise:
 my_tuple = ('taco', 'mashed potatoes', 'beef stew', 'french toast','chef salad')
 for item in my_tuple:
-   print(item)
+   print(f"Original Menu Item:", item)
 copied_tuple = tuple(my_tuple)
 temporary_list = list(copied_tuple)
 temporary_list[0] = "french fries"
 temporary_list[3] = "apple pie"
 copied_tuple = tuple(temporary_list)
 for item in copied_tuple:
-      print(item)
+      print(f"Revised Menu Item:", item)
