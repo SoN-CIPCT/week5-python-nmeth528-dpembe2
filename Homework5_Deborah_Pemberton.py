24bc8b533800e515cf546b744fd526e1bf748ac2
dpembe2 <dpembe2@uw.edu>
2026-02-03 21:33:37 -0800

    Update Homework5_Deborah_Pemberton.py

--- Homework5_Deborah_Pemberton.py
+++ Homework5_Deborah_Pemberton.py
@@ -2,3 +2,7 @@ cats =  ['Siamese', 'Tabby', 'Maine Coon', 'Ragdoll', 'American Shorthair', 'Aby
 print(cats)
 first_two = cats[:2]
 print(first_two)
+list_length = len(cats)
+middle_index = list_length // 2
+middle_two_items = cats[middle_index - 1 : middle_index + 1]
+print(middle_two_items)
